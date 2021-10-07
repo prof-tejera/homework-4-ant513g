a1517903d7510843b783a663eb59fb39117bcc5c
ant513g <89939545+ant513g@users.noreply.github.com>
2021-10-06 23:56:25 -0400

    Updated styling

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,6 +3,29 @@ import { Component } from "react";
 import Number from "./Number";
 import Operator from "./Operator";
 import Screen from "./Screen";
+import styled from 'styled-components';
+import PropTypes from 'prop-types'
+
+
+
+const Container = styled.div`
+  padding: 24px 20px;
+  border-radius: 16px;
+  width: min-content;
+  overflow: auto;
+  background-color: #2D3142;
+  box-shadow: 4px 4px 16px 0 rgba(0, 0, 20, 0.25);
+`;
+
+const NumWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+`;
+
+const OpWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+`;
 
 class Calculator extends Component {
   state = {
@@ -32,6 +55,10 @@ class Calculator extends Component {
         this.setState({ operator: null, first: first - second, second: null });
       } else if (this.state.operator === "x") {
         this.setState({ operator: null, first: first * second, second: null });
+      } else if (this.state.operator === ".") {
+        this.setState({ operator: null, first: first . second, second: null });
+      } else if (this.state.operator === "+/-") {
+        this.setState({ operator: null, first: -first, second: null });
       }
     } else if (operator === "clear") {
       this.setState({ first: null, second: null, operator: null });
@@ -44,30 +71,36 @@ class Calculator extends Component {
 
   render() {
     return (
-      <>
+      <><Container>
         <Screen value={this.getScreenValue()} />
         <div style={{ display: "flex" }}>
-          <div>
-            <Number value={0} onClick={this.handleNumberClick} />
-            <Number value={1} onClick={this.handleNumberClick} />
-            <Number value={2} onClick={this.handleNumberClick} />
-            <Number value={3} onClick={this.handleNumberClick} />
-            <Number value={4} onClick={this.handleNumberClick} />
-            <Number value={5} onClick={this.handleNumberClick} />
-            <Number value={6} onClick={this.handleNumberClick} />
+          <NumWrapper>
+            {/* Changed the Order of the numbers */}
             <Number value={7} onClick={this.handleNumberClick} />
             <Number value={8} onClick={this.handleNumberClick} />
             <Number value={9} onClick={this.handleNumberClick} />
-          </div>
+            <Number value={4} onClick={this.handleNumberClick} />
+            <Number value={5} onClick={this.handleNumberClick} />
+            <Number value={6} onClick={this.handleNumberClick} />
+            <Number value={1} onClick={this.handleNumberClick} />
+            <Number value={2} onClick={this.handleNumberClick} />
+            <Number value={3} onClick={this.handleNumberClick} />
+            <Number value={0} onClick={this.handleNumberClick} />
+          </NumWrapper>
           <div style={{ paddingLeft: 10 }}>
-            <Operator value="+" onClick={this.handleOperatorClick} />
-            <Operator value="/" onClick={this.handleOperatorClick} />
-            <Operator value="x" onClick={this.handleOperatorClick} />
-            <Operator value="-" onClick={this.handleOperatorClick} />
-            <Operator value="=" onClick={this.handleOperatorClick} />
-            <Operator value="clear" onClick={this.handleOperatorClick} />
+            <OpWrapper>
+              <Operator value="+/-" onClick={this.handleOperatorClick} />
+              <Operator value="clear" onClick={this.handleOperatorClick} />
+              <Operator value="+" onClick={this.handleOperatorClick} />
+              <Operator value="-" onClick={this.handleOperatorClick} />
+              <Operator value="/" onClick={this.handleOperatorClick} />
+              <Operator value="x" onClick={this.handleOperatorClick} />
+              <Operator value="." onClick={this.handleOperatorClick} />
+              <Operator value="=" onClick={this.handleOperatorClick} />
+            </OpWrapper>
           </div>
         </div>
+        </Container>
       </>
     );
   }
--- src/components/Operator.js
+++ src/components/Operator.js
@@ -1,20 +1,60 @@
 import { Component } from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
-const Container = styled.div`
-  padding: 10px;
-  border: 1px solid black;
-  width: 60px;
+const Container = styled.button`
+  border: none;
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  overflow: auto;
+  padding: 16px;
+  margin: 4px;
+  height: 50px;
+  width: 50px;
+  box-sizing: border-box;
+  border-radius: 16px;
+  text-align: center;
+  box-shadow: inset  ${props => (props.pressed ? '0 0 8px rgba(5, 5, 5, 0.95)' : '0 0 16px #F06124')};
+  background-color: ${props => (props.pressed ? '#F06124' : '#EF8354')};
+  
 `;
 
 class Operator extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pressed: false,
+    };
+  }
+  onMouseDown = () => {
+    this.setState({
+          pressed: true,
+    });
+    this.props.onClick(this.props.value);
+  }
+  onMouseUp = () => {
+    this.setState({
+          pressed: false,
+    });
+  }
+
   render() {
+      // const { pressed, backgroundColor} = this.state;
+      
     return (
-      <Container onClick={() => this.props.onClick(this.props.value)}>
+      <Container
+        pressed={this.state.pressed}
+        onMouseDown={this.onMouseDown}
+        onMouseUp={this.onMouseUp}
+      > 
         {this.props.value}
       </Container>
-    );
+      );
   }
 }
 
+Operator.propTypes = {
+  value: PropTypes.node,
+}
 export default Operator;
